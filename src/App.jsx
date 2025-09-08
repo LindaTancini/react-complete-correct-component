51eab059da18aba88f2cc79825ca24df37db5c3d
LindaTancini <lindatancini95@gmail.com>
2025-09-08 14:18:27 +0200

    feat: complete todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { MyContext } from "./Context/MyContext";
-import UsersList from "./components/UsersList";
+import UsersList from "./components/UserList";
 import UserDetails from "./components/UserDetails";
 
 function App() {
   // TODO: stato per gli utenti
   // const [____, ____] = useState([]);
+  const [users, setUsers] = useState([]);
 
   // TODO: useEffect per fare fetch da "https://jsonplaceholder.typicode.com/users"
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error("Errore fetch:", err));
+  }, []);
 
   return (
     <MyContext.Provider
-      value={
-        {
-          /* TODO: inserisci utenti qui */
-        }
-      }
+      value={{
+        users /* TODO: inserisci utenti qui */,
+      }}
     >
       <Router>
         <nav>
@@ -25,7 +30,10 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/users" element={<UsersList /* TODO: props */ />} />
+          <Route
+            path="/users"
+            element={<UsersList users={users} /* TODO: props */ />}
+          />
           <Route path="/users/:id" element={<UserDetails />} />
         </Routes>
       </Router>
